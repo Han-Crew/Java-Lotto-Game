4cf6eb517770ba85a66720cb92ad2e50d53e0b60
woobeen <woobeen.lim@kurlycorp.com>
2021-04-26 19:37:21 +0900

    fix - expression 유효성 검사 수정 , SplitStrategy 함수형 인터페이스 추가(구현x)

--- src/test/java/calculator/StringSplitterTest.java
+++ src/test/java/calculator/StringSplitterTest.java
@@ -1,5 +1,19 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
 public class StringSplitterTest {
 
+    @Nested
+    @DisplayName("일반적인 문자열 계산식 자르기")
+    class ExpressionSplit {
+
+    }
+
+    @Nested
+    @DisplayName("커스텀 문자열 계산식 자르기")
+    class CustomExpressionSplit {
+
+    }
 }
