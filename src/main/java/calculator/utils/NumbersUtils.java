d3f16e769cef68a14082a01abe3ced2891c081cb
limwoobin <drogba02@naver.com>
2021-05-02 15:55:29 +0900

    add - 테스트 코드 추가
    CalculatorTest - 계산식 테스트
    ExpressionTest - 문자열 관련 테스트
    NumbersTest - 숫자형식 관련 테스트
    StringSplitterTest - 문자열 자르기 및 팩토리 관련 테스트

--- src/main/java/calculator/utils/NumbersUtils.java
+++ src/main/java/calculator/utils/NumbersUtils.java
@@ -3,7 +3,7 @@ package calculator.utils;
 import java.util.List;
 
 public class NumbersUtils {
-    public static void validateNumberSize(List<Integer> numbers) {
+    public static void validateNumbers(List<Integer> numbers) {
         numbers.forEach((number) -> {
             if (number < 0 || number > 9) {
                 throw new RuntimeException();
